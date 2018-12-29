ce7a6250129dc6e881dd265b5c68214943af3c65
OmegaRogue <philipp@buennagel.com>
2018-12-29 14:47:37 +0100

    moving away from hooks

--- OmegaMod/API/ModConfigurator.cs
+++ OmegaMod/API/ModConfigurator.cs
@@ -4,22 +4,18 @@ namespace API
 {
 	public class ModConfigurator : MonoBehaviour
 	{
-		void OnGUI()
+		private void OnGUI()
 		{
 			// Make a background box
-			GUI.Box(new Rect(10,10,100,90), "Loader Menu");
-    
+			GUI.Box(new Rect(10, 10, 100, 90), "Loader Menu");
+
 			// Make the first button. If it is pressed, Application.Loadlevel (1) will be executed
-			if(GUI.Button(new Rect(20,40,80,20), "Level 1"))
+			if (GUI.Button(new Rect(20, 40, 80, 20), "Level 1"))
 			{
-				Application.LoadLevel(1);
 			}
-    
+
 			// Make the second button.
-			if(GUI.Button(new Rect(20,70,80,20), "Level 2")) 
-			{
-				Application.LoadLevel(2);
-			}
+			if (GUI.Button(new Rect(20, 70, 80, 20), "Level 2")) Application.LoadLevel(2);
 		}
 	}
 }
